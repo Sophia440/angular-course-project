861e0807dc8410fd8159cd646e4c8914e1b7cad7
sklimovich <klimovi4.sophia@gmail.com>
2022-08-07 23:31:27 +0200

    - Add communication between components through databinding - Update the recipe list to show the details of a particular recipe on click - Update the add button on the shopping edit screen

--- recipe-app/src/app/shopping-list/shopping-list.component.ts
+++ recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Ingredient} from "../shared/ingredient.model";
 
 @Component({
@@ -12,9 +12,14 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient('Egg', 2)
   ]
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
   }
 
 }
